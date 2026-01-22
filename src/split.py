c38422f37a4285185268c0978bccbfea63d95a8e
Abhishek Karyagol <akaryagol@gmail.com>
2026-01-22 09:49:00 +0530

    clear

--- src/split.py
+++
@@ -1,7 +0,0 @@
-def time_based_split(df, split_ratio=0.8):
-    split_index = int(len(df) * split_ratio)
-
-    train = df.iloc[:split_index]
-    test = df.iloc[split_index:]
-
-    return train, test
